d880be1db85598cc9f644ef76d46f16691aaef03
Nulle995 <nullewfn@gmail.com>
2024-11-22 14:33:23 -0600

    added env variables to the frontend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,11 +3,11 @@ import { ACCESS_TOKEN, REFRESH_TOKEN } from "./constants.js";
 import { auth } from "./utils/authorization.js";
 
 export const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/",
+  baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:8000/",
 });
 
 export const APIToken = axios.create({
-  baseURL: "http://127.0.0.1:8000/",
+  baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:8000/",
 });
 
 APIToken.interceptors.request.use(
