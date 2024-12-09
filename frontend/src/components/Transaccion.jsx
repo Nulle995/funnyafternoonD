32a8e2bdcd61d85013a4ad24596de865f6ff1611
Nulle995 <nullewfn@gmail.com>
2024-12-09 19:58:26 -0300

    added the Edit functionallity to Transaccion component

--- frontend/src/components/Transaccion.jsx
+++ frontend/src/components/Transaccion.jsx
@@ -5,26 +5,32 @@ import { toast, Toaster } from "sonner";
 import formatDate from "../utils/formatDate";
 import formatPrice from "../utils/formatPrice";
 import Dialog from "./Dialog";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 const Transaccion = ({ transs, handleDelete }) => {
-  console.log(transs);
-
   const {
     pk,
-    fecha,
-    categoria,
-    desc,
-    monto,
+    fecha: estadoFecha,
+    categoria: estadoCategoria,
+    desc: estadoDesc,
+    monto: estadoMonto,
     apoderado,
     apoderado_desc,
     evento,
     evento_desc,
-    tipo_transaccion,
+    tipo_transaccion: estadoTransaccion,
   } = transs;
+
+  const [tipo_transaccion, setTipoTransaccion] = useState(estadoTransaccion);
+  const [fecha, setFecha] = useState(estadoFecha);
+  const [categoria, setCategoria] = useState(estadoCategoria);
+  const [desc, setDesc] = useState(estadoDesc);
+  const [monto, setMonto] = useState(estadoMonto);
+
   const formattedPrice = formatPrice(monto);
   const formattedDate = formatDate(fecha);
   const dialogRef = useRef(null);
+  const editDialogRef = useRef(null);
   const moneda = tipo_transaccion === "Ingreso" ? "💵" : "⛔";
   const color = tipo_transaccion === "Ingreso" ? "green" : "red";
   const signo = tipo_transaccion === "Ingreso" ? "+" : "-";
@@ -40,6 +46,32 @@ const Transaccion = ({ transs, handleDelete }) => {
   const handleAlertDelete = () => {
     handleDelete(pk);
     toggleDialog();
+  };
+
+  const toggleEditDialog = (e) => {
+    if (!editDialogRef.current) return;
+
+    editDialogRef.current.hasAttribute("open")
+      ? editDialogRef.current.close()
+      : editDialogRef.current.showModal();
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      const res = APIToken.patch(`transacciones/${pk}/update/`, {
+        tipo_transaccion,
+        fecha,
+        categoria,
+        desc,
+        monto,
+      });
+      const data = res.data;
+      toggleEditDialog();
+      console.log(data);
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   return (
@@ -89,6 +121,72 @@ const Transaccion = ({ transs, handleDelete }) => {
           </button>
         </div>
         <Toaster richColors />
+        <Dialog ref={editDialogRef} toggleDialog={toggleEditDialog}>
+          <form action="" onSubmit={handleSubmit} style={{ display: "grid" }}>
+            <label htmlFor="fecha">Fecha: </label>
+            <input
+              type="date"
+              name="fecha"
+              id="fecha"
+              value={fecha}
+              onChange={(e) => setFecha(e.target.value)}
+            />
+            <label htmlFor="categoria">Categoría: </label>
+            <select
+              name="categoria"
+              id="categoria"
+              value={categoria}
+              onChange={(e) => setCategoria(e.target.value)}
+            >
+              <option value="Pago de Plan">Pago de Plan</option>
+              <option value="Utensilios">Utensilios</option>
+              <option value="Arriendo Local">Arriendo Local</option>
+              <option value="Servicios">Servicios</option>
+            </select>
+            <label htmlFor="desc">Descripción: </label>
+            <input
+              type="text"
+              name="desc"
+              id="desc"
+              value={desc}
+              onChange={(e) => setDesc(e.target.value)}
+            />
+            <label htmlFor="monto">Monto: </label>
+            <input
+              type="number"
+              name="monto"
+              id="monto"
+              value={monto}
+              onChange={(e) => setMonto(e.target.value)}
+            />
+            <label htmlFor="tipo_transaccion">Tipo: </label>
+            <select
+              name="tipo_transaccion"
+              id="tipo_transaccion"
+              value={tipo_transaccion}
+              onChange={(e) => setTipoTransaccion(e.target.value)}
+            >
+              <option value="Ingreso">Ingreso</option>
+              <option value="Egreso">Egreso</option>
+            </select>
+            <div>
+              <button>Aceptar</button>
+              <button
+                type="button"
+                onClick={() => {
+                  setTipoTransaccion(estadoTransaccion);
+                  setFecha(estadoFecha);
+                  setCategoria(estadoCategoria);
+                  setDesc(estadoDesc);
+                  setMonto(estadoMonto);
+                  toggleEditDialog();
+                }}
+              >
+                Cancelar
+              </button>
+            </div>
+          </form>
+        </Dialog>
         <Dialog ref={dialogRef} toggleDialog={toggleDialog}>
           Desea borrar
           <button onClick={handleAlertDelete}>Sí</button>
